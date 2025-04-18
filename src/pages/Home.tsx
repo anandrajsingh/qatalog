b364e6d1c2c42b9b94bacf1ebc6f3ca0880580c2
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-04-18 19:27:53 +0530

    main section complete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,6 +18,12 @@ export const Home = () => {
         { logo: "/microsoft-teams.png", url: "teams.microsoft.com", description: "Microsoft Teams enables communication, meetings, collaborations across organizations." },
     ]
 
+    const plans = [
+        { logo: "/pro.png", price: "15", user: "per", name: "Pro", description: "For scaling teams that need instant access to accurate and up-to-date information.", buttonText: "Start Your Trial 14 Days", includes: "", includeItems: ["Unlimited keyword search", "Unlimited AI-powered queries", "20+ app connections", "Unlimited team members", "Rolling monthly contract", "Upgrade or cancel anytime"] },
+        { logo: "/pro-plus.png", price: "25", user: "per", name: "Pro-plus", description: "For enterprises needing advanced features, customization, and full control.", buttonText: "Book a Demo", includes: " Pro, plus", includeItems: ["Audit logs, query controls", "Analytics & feedback data", "Priority support", "Dedicated success manager", "Personalized onboarding", "Service level agreement", "Prioritized integration requests"] },
+        { logo: "/Enterprise.png", price: "35", user: "2", name: "Enterprise", description: "For established organizations in need of extra flexibity and control", buttonText: "Book a Demo", includes: "enterprise", includeItems: ["Audit logs, query controls", "Analytics & feedback data", "Priority support", "Dedicated success manager", "Personalized onboarding", "Service level agreement", "Prioritized integration requests"] },
+    ]
+
     return (
         <div className="min-h-screen w-full font-sans items-center flex flex-col bg-black">
             <div className="flex flex-col w-7/10">
@@ -91,10 +97,49 @@ export const Home = () => {
                                         </a>
                                     </div>
 
-                                    <p className="text-xl text-white">{dataSource.url}</p>
-                                    <p className="text-sm text-white">{dataSource.description}</p>
+                                    <p className="text-xl text-white w-full text-start mt-15">{dataSource.url}</p>
+                                    <p className="text-sm text-white w-full mt-5">{dataSource.description}</p>
                                 </div>
                             ))}
+                        </div>
+                    </section>
+
+                    <section className="flex flex-col items-center justify-center w-full my-20">
+                        <div className="flex justify-between items-center w-full">
+                            <div className="text-5xl font-semibold w-2xl">Flexible Pricing Plans for Every Team and Business Size</div>
+                            <div className="text-xl w-lg">Choose the plan that fits your needs, from startups enterprises, with scalable, affordable options.</div>
+                        </div>
+                        <div className="grid grid-cols-2 md:grid-cols-3 gap-6 w-full my-20">
+                            {plans.map((plan, index) => (
+                                <div key={index} className="bg-gradient-to-b from-[#1f1f1f] to-[#0d0d0d] rounded-2xl p-6 text-center border border-gray-800 hover:shadow-lg transition">
+                                    <div className="flex justify-between items-center">
+                                        <div className="bg-gradient-to-br from-gray-500 to-black border-gray-500 p-3 border-t border-l rounded-full">
+                                            <img src={plan.logo} alt="" className="w-8 h-8" />
+                                        </div>
+                                        <div className="text-2xl font-semibold">
+                                            ${plan.price}/<span className="text-sm font-normal">mo {plan.user} user</span>
+                                        </div>
+                                    </div>
+                                    <div className="w-full text-start text-2xl font-semibold mt-10">{plan.name}</div>
+                                    <div className="w-full text-start mt-6">{plan.description}</div>
+                                    <div className="w-full text-center mt-6 bg-white text-black py-3 rounded-full text-lg font-medium">{plan.buttonText}</div>
+                                    <div className="w-full text-start mt-6 font-medium">Includes{plan.includes}:</div>
+                                    <div className="w-full text-start mt-6 flex flex-col gap-4">{plan.includeItems.map((items, index) => (
+                                        <div key={index} className="flex gap-2 text-sm">
+                                            <img src="/correct.png" alt="" className="filter invert w-4 h-4" />
+                                            {items}
+                                        </div>
+                                    ))}</div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="px-6 py-20 bg-gradient-to-r from-[#0d0d0d] via-[#1f1f1f] to-[#0d0d0d] border border-gray-700 rounded-xl w-full mx-auto text-center">
+                            <h4 className="text-5xl font-medium mb-4">Start Seamless Collaboration</h4>
+                            <p className="text-white/80 mb-6">Choose a pricing plan that fits your needs. Scalable options for businesses of all sizes with flexible features.</p>
+                            <div className="flex gap-4 justify-center">
+                                <button className="px-6 py-2 bg-white text-black rounded-full font-medium">Started For Free</button>
+                                <button className="px-6 py-2 bg-zinc-700 text-white rounded-full font-medium">Book a Demo</button>
+                            </div>
                         </div>
                     </section>
                 </main>
